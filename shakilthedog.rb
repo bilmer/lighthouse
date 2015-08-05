61a1d026a2b649bbae8c2f07c891653c804deb23
bilmer <jessicabilmer@gmail.com>
2015-08-04 21:02:09 -0700

    FINAL 9PM
    Done lecture 5

--- shakilthedog.rb
+++ shakilthedog.rb
@@ -1,22 +1,24 @@
 
-puts "Say something to the dog!"
-response = gets.chomp.downcase
+begin
 
-if response == "woof"
-  puts "WOOF, WOOF, WOOF"
-elsif response == "shakil, stop!"
-  puts " "
-elsif response == "meow"
-  puts "WOOF, WOOF, WOOF, WOOF, WOOF"
-elsif response.include? "treat"
-  puts "..."
-elsif response == "go away"
-  puts "*Shakil leaves room*"
-else
-  puts ""
-end
+  puts "Say something to the dog!"
+  response = gets.chomp.downcase
 
-  
+  if response == "woof"
+    puts "WOOF, WOOF, WOOF"
+  elsif response == "shakil, stop!"
+    puts " "
+  elsif response == "meow"
+    puts "WOOF, WOOF, WOOF, WOOF, WOOF"
+  elsif response.include? "treat"
+    puts "..."
+  elsif response == "go away"
+    puts "*Shakil leaves room*"
+  else
+    puts ""
+  end
+
+end while response != 'exit'  
 
 
 
